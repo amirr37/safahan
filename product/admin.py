86d16a7c6f358a66d54861e82fa37d18a6884788
Amir Jahangiri <amirrj037@gmail.com>
2023-11-26 00:39:27 +0330

    add order and orderItem to admin panel

--- product/admin.py
+++ product/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from product.models import Category, Product
+from product.models import Category, Product, Order, OrderItem
 
 
 @admin.register(Category)
@@ -14,4 +14,21 @@ class ProductAdmin(admin.ModelAdmin):
     search_fields = ('name', 'category__name')  # Add a search field for name and category name
 
 
+class OrderItemInline(admin.TabularInline):
+    model = OrderItem
 
+
+@admin.register(Order)
+class OrderAdmin(admin.ModelAdmin):
+    list_display = ('id', 'user', 'order_date', 'total_amount', 'is_completed')
+    list_filter = ('user', 'is_completed')
+    search_fields = ('user__username', 'user__first_name', 'user__last_name', 'id')
+    inlines = [OrderItemInline]
+
+
+@admin.register(OrderItem)
+class OrderItemAdmin(admin.ModelAdmin):
+    list_display = ('id', 'order', 'product', 'quantity', 'subtotal')
+    list_filter = ('order__user', 'product')
+    search_fields = (
+        'order__user__username', 'order__user__first_name', 'order__user__last_name', 'product__name', 'id')
