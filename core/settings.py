3983b296c0ea6f74ea7150b935a09406dbbe6445
Amir Jahangiri <amirrj037@gmail.com>
2023-11-26 00:57:32 +0330

    display user model in admin panel

--- core/settings.py
+++ core/settings.py
@@ -85,7 +85,6 @@ DATABASES = {
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
 
-AUTH_USER_MODEL = 'account.CustomUser'
 
 AUTH_PASSWORD_VALIDATORS = [
     {
@@ -122,3 +121,5 @@ STATIC_URL = "static/"
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+AUTH_USER_MODEL = 'account.CustomUser'
