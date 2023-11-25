8c579572f5ecae9a119de0cbedb2517d35d1be0c
Amir Jahangiri <amirrj037@gmail.com>
2023-11-25 19:53:52 +0330

    bring models of product from account app to product app

--- account/models.py
+++ account/models.py
@@ -2,23 +2,3 @@ from django.db import models
 
 
 # Create your models here.
-class Category(models.Model):
-    name = models.CharField(max_length=100)
-    description = models.TextField()
-
-    def __str__(self):
-        return self.name
-
-
-class Product(models.Model):
-    name = models.CharField(max_length=100)
-    description = models.TextField()
-    price = models.DecimalField(max_digits=10, decimal_places=2)
-    category = models.ForeignKey(Category, on_delete=models.CASCADE)
-    image = models.ImageField(upload_to='products/', blank=True, null=True)
-    weight = models.DecimalField(max_digits=6, decimal_places=2, blank=True, null=True)
-    stock = models.IntegerField()
-    tags = models.CharField(max_length=200, blank=True)
-
-    def __str__(self):
-        return f"{self.name} ({self.category.name})"
