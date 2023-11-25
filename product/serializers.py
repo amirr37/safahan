efb9990ba9f588e339853b39be1873f8c1aeff2e
Amir Jahangiri <amirrj037@gmail.com>
2023-11-26 02:15:35 +0330

    create category serializer

--- product/serializers.py
+++ product/serializers.py
@@ -1,1 +1,12 @@
 from rest_framework import serializers
+
+from product.models import Category
+
+
+class CategorySerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Category
+        fields = '__all__'
+
+
+
