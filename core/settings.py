a1d606e0030ca161622a595bd9e76e139eac16b4
Amir Jahangiri <amirrj037@gmail.com>
2023-11-27 23:30:50 +0330

    add swagger to project

--- core/settings.py
+++ core/settings.py
@@ -43,6 +43,8 @@ INSTALLED_APPS = [
     # external apps
     'rest_framework',
     'rest_framework.authtoken',
+    'drf_spectacular',
+
 ]
 
 MIDDLEWARE = [
@@ -137,5 +139,14 @@ REST_FRAMEWORK = {
     ],
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
     'PAGE_SIZE': 10,  # Set the number of items per page according to your preference
+    'DEFAULT_SCHEMA_CLASS': 'drf_spectacular.openapi.AutoSchema',
 
+}
+
+SPECTACULAR_SETTINGS = {
+    'TITLE': 'Your Project API',
+    'DESCRIPTION': 'Your project description',
+    'VERSION': '1.0.0',
+    'SERVE_INCLUDE_SCHEMA': False,
+    # OTHER SETTINGS
 }
