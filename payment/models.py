dac2b646e84919ac4743a918ebed1b66f0a03361
amirrj037 <amirrj037@gmail.com>
2023-12-30 14:22:35 +0330

    add transaction model

--- payment/models.py
+++ payment/models.py
@@ -1,3 +1,22 @@
 from django.db import models
 
+from account.models import CustomUser
+
+
 # Create your models here.
+
+
+class Transaction(models.Model):
+    user = models.ForeignKey(CustomUser, on_delete=models.CASCADE, related_name='transactions')
+    amount = models.IntegerField()
+    description = models.TextField(blank=True, null=True)
+    ref_id = models.TextField(blank=True, null=True)
+    authority = models.TextField()
+    status = models.IntegerField(blank=True, null=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        db_table = 'transaction'
+
+
